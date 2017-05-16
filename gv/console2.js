aeef837e57871cb66d242afe35470382504041b1
fakegit <fakegit@outlook.com>
2017-05-17 00:12:39 +0800

    Create console2.js

---
+++ gv/console2.js
@@ -0,0 +1,11 @@
+// open firefox/chrome
+// go to http://voice.google.com
+// visit legacy google voice
+// click add new number
+// open inspect, go to network option
+// select a number and click continue
+// find the request from the network option panel, right click, choose "copy as curl(bash)"
+// replace 'REPLACE' with the copy and save
+// run this script on browser's console panel
+
+while true; do REPLACE ;sleep 1.5s; done

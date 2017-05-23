bea099a3d15f36b44316648edbdfe000d8352553
fakegit <fakegit@outlook.com>
2017-05-23 21:02:23 +0800

    Create console3.js

---
+++ gv/console3.js
@@ -0,0 +1,57 @@
+// open firefox/chrome
+// go to http://voice.google.com
+// visit legacy google voice
+// click add new number
+// select a number
+// run this script on browser's console panel
+
+var sleep = delay => new Promise(resolve => setTimeout(resolve, delay));
+
+var composeClick = function x(btn) {
+  var rect = btn.getBoundingClientRect();
+  var x = rect.left + rect.width * Math.random();
+  var y = rect.top + rect.height * Math.random();
+  const mousedown = new MouseEvent("mousedown", {
+    screenX: x + window.screen.availLeft,
+    screenY: y + window.screen.availTop,
+    clientX: x,
+    clientY: y,
+  });
+  const click = new MouseEvent("click", {
+    screenX: x + window.screen.availLeft,
+    screenY: y + window.screen.availTop,
+    clientX: x,
+    clientY: y,
+  });
+  const mouseup = new MouseEvent("mouseup", {
+    screenX: x + window.screen.availLeft,
+    screenY: y + window.screen.availTop,
+    clientX: x,
+    clientY: y,
+  });
+  btn.dispatchEvent(mousedown);
+  return sleep(150 + Math.random() * 30)
+    .then(() => {
+      btn.dispatchEvent(click);
+      return sleep(30 + Math.random() * 30);
+    }).then(() => {
+      btn.dispatchEvent(mouseup);
+    });
+}
+
+function task() {
+  var btn = document.querySelector(".continueButton");
+  if (!btn) {
+    alert("Finish");
+    return;
+  }
+  composeClick(btn)
+    .then(() => {
+      // 在此调整点击时间间隔
+      return sleep(500 + Math.random() * 3000);
+    })
+    .then(() => {
+      task();
+    });
+}
+task();

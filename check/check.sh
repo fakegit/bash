bf62482b31ec81fbe4fe4f2a66d45d715360860f
fakegit <fakegit@outlook.com>
2017-07-23 01:33:58 +0800

    Update check.sh

--- check/check.sh
+++ check/check.sh
@@ -1,5 +1,28 @@
 #!/bin/sh
 
-echo "0 0 * * * python hostloc.py" >> /etc/crontab
-echo "0 0 * * * python v2ex.py" >> /etc/crontab
-service cron restart
+if [ ! -f "hostloc.py" ]; then
+  wget https://github.com/fakegit/bash/raw/master/hostloc.py
+fi
+if [ ! -f "v2ex.py" ]; then
+  wget https://github.com/fakegit/bash/raw/master/v2ex.py
+fi
+
+echo -e "Hostloc Account Info"
+stty erase '^H' && read -p "Username: " hostloc_username
+sed -i "s/username='/username='${hostloc_username}/g" hostloc.py
+stty erase '^H' && read -s -p "Password: " hostloc_password
+sed -i "s/passwd='/passwd='${hostloc_password}/g" hostloc.py
+
+echo -e "V2ex Account Info"
+stty erase '^H' && read -p "Username: " v2ex_username
+sed -i "s/username='/username='${v2ex_username}/g" v2ex.py
+stty erase '^H' && read -s -p "Password: " v2ex_password
+sed -i "s/passwd='/passwd='${v2ex_password}/g" v2ex.py
+
+#pip install requests
+#python hostloc.py
+#python v2ex.py
+
+#echo "0 0 * * * python hostloc.py" >> /etc/crontab
+#echo "0 0 * * * python v2ex.py" >> /etc/crontab
+#service cron restart

60169a9e6341adef1482ce690604f254533ae511
fakegit <fakegit@outlook.com>
2017-08-18 00:22:00 +0800

    Update te.sh

--- hitleap/te.sh
+++ hitleap/te.sh
@@ -13,7 +13,10 @@ unzip kilohits.zip
 mv kilohits.com-viewer* Kilohits
 rm -rf "kilohits.zip"
 apt-get install -y libnss3-dev
+# ~/te/kilohits/kilohits.com-viewer
 
 wget -O "otohits.zip" http://www.otohits.net/dl/OtohitsApp_3107_Linux.zip
 unzip otohits.zip
 rm -rf "otohits.zip"
+apt-get install -y libcurl4-openssl-dev
+# ~/te/OtohitsApp/OtohitsApp

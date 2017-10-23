5f8f6c231eb5626dab357e47c4a271a048f8cd90
fakegit <fakegit@outlook.com>
2017-10-23 22:40:24 +0800

    Update 3proxy.sh

--- misc/3proxy.sh
+++ misc/3proxy.sh
@@ -1,32 +1,42 @@
 #!/bin/bash
 
 # Creating temporary working directory
-mkdir /tmp/proxy
-cd /tmp/proxy
+mkdir /tmp/3proxy
+cd /tmp/3proxy
 
 # Downloading latest stable 3proxy 0.8.11
 wget --no-check-certificate https://github.com/z3APA3A/3proxy/archive/0.8.11.tar.gz
 
 # Extracting archive
-tar -xvzf 3proxy-0.8.11.tar.gz
+tar -xvzf 0.8.11.tar.gz
 
 # Compiling
-cd 3proxy/
+cd 3proxy-0.8.11
 make -f Makefile.Linux
 
 # Moving executable file to current user home directory
 cd src/
-mv 3proxy $HOME
+# mv 3proxy $HOME
+
+# Fixing permission on files
+# chmod +x 3proxy
+
+mv 3proxy /usr/bin
+chmod +x /usr/bin/3proxy
 
 # Downloading sample configuration to current user home directory
 cd $HOME
 wget --no-check-certificate https://raw.githubusercontent.com/fakegit/bash/master/misc/3proxy.cfg
 
-# Fixing permission on files
-chmod +x 3proxy
+
 
 # Removing temporary working directory from tmp
-rm -rf /tmp/proxy
+rm -rf /tmp/3proxy
 
 # Creating folder for logs or otherwise 3proxy won't work
 mkdir /var/log/3proxy
+
+# /usr/bin/3proxy $HOME/3proxy.cfg
+# ps ax | grep 3proxy
+# kill -9 pid
+

93142b4631e6c2b15904ba92bae73cecd62dae05
fakegit <fakegit@outlook.com>
2017-10-25 22:24:37 +0800

    Update te.sh

--- hitleap/te.sh
+++ hitleap/te.sh
@@ -3,30 +3,45 @@
 # apt-get upgrade -y # To get the latest package lists
 # apt-get install nano xorg xrdp jwm firefox chromium-browser flashplugin-installer -y
 # service xrdp restart
-apt-get install vim screen unzip -y
+apt-get install vim screen unzip firefox -y
+apt-get install -y libnss3-dev
+apt-get install -y libcurl4-openssl-dev
 
-cd ~
-mkdir te
-cd ~/te
+te_dir=$HOME/te
+kh_dir=$HOME/te/Kilohits
+oh_dir=$HOME/te/Otohits
+
 # kilohits
+mkdir -p $kh_dir
+cd $kh_dir
 wget -O "kilohits.zip" https://www.dropbox.com/s/akkpgc0jzck5eho/kilohits.com-viewer-linux-x64.zip?dl=1
 unzip kilohits.zip
-mv kilohits.com-viewer* Kilohits
+mv kilohits.com-viewer*/* .
+rm -rf kilohits.com-viewer-*
 rm -rf "kilohits.zip"
-apt-get install -y libnss3-dev
 # ~/te/Kilohits/kilohits.com-viewer
+
+cat > $HOME/kilohits_restart.sh << EOF #!/bin/sh
+ps -ef | grep -i kilohits | grep -v grep | cut -c 9-15 | xargs kill -s 9 > /dev/null 2>&1
+$kh_dir/kilohits.com-viewer > /dev/null 2>&1 &
+EOF
+
+chmod +x $HOME/kilohits_restart.sh
+
+echo "$HOME/kilohits_restart.sh" >> /etc/rc.local
+echo "* /2 * * * $HOME/kilohits_restart.sh" >> /etc/crontab
 
 wget -O "otohits.zip" http://www.otohits.net/dl/OtohitsApp_3107_Linux.zip
 unzip otohits.zip
 rm -rf "otohits.zip"
-apt-get install -y libcurl4-openssl-dev
 # ~/te/OtohitsApp/OtohitsApp
 
-cat > $HOME/kilohits_restart.sh << EOF #!/bin/sh
-ps -ef | grep kilohits | grep -v grep | cut -c 9-15 | xargs kill -s 9 > /dev/null 2>&1
-/usr/bin/3proxy $HOME/3proxy.cfg > /dev/null 2>&1 &
+cat > $HOME/otohits_restart.sh << EOF #!/bin/sh
+ps -ef | grep -i kilohits | grep -v grep | cut -c 9-15 | xargs kill -s 9 > /dev/null 2>&1
+$kh_dir/kilohits.com-viewer > /dev/null 2>&1 &
 EOF
-chmod +x $HOME/3proxy_restart.sh
 
-echo "$HOME/3proxy_restart.sh" >> /etc/rc.local
-echo "* /24 * * * $HOME/3proxy_restart.sh" >> /etc/crontab
+chmod +x $HOME/otohits_restart.sh
+
+echo "$HOME/otohits_restart.sh" >> /etc/rc.local
+#echo "* /2 * * * $HOME/otohits_restart.sh" >> /etc/crontab

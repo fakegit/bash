167caf12415557cfa534a84834b880916689b427
fakegit <fakegit@outlook.com>
2017-10-25 20:22:30 +0800

    Update te.sh

--- hitleap/te.sh
+++ hitleap/te.sh
@@ -3,6 +3,7 @@
 # apt-get upgrade -y # To get the latest package lists
 # apt-get install nano xorg xrdp jwm firefox chromium-browser flashplugin-installer -y
 # service xrdp restart
+apt-get install vim screen unzip -y
 
 cd ~
 mkdir te
@@ -13,10 +14,19 @@ unzip kilohits.zip
 mv kilohits.com-viewer* Kilohits
 rm -rf "kilohits.zip"
 apt-get install -y libnss3-dev
-# ~/te/kilohits/kilohits.com-viewer
+# ~/te/Kilohits/kilohits.com-viewer
 
 wget -O "otohits.zip" http://www.otohits.net/dl/OtohitsApp_3107_Linux.zip
 unzip otohits.zip
 rm -rf "otohits.zip"
 apt-get install -y libcurl4-openssl-dev
 # ~/te/OtohitsApp/OtohitsApp
+
+cat > $HOME/kilohits_restart.sh << EOF #!/bin/sh
+ps -ef | grep kilohits | grep -v grep | cut -c 9-15 | xargs kill -s 9 > /dev/null 2>&1
+/usr/bin/3proxy $HOME/3proxy.cfg > /dev/null 2>&1 &
+EOF
+chmod +x $HOME/3proxy_restart.sh
+
+echo "$HOME/3proxy_restart.sh" >> /etc/rc.local
+echo "* /24 * * * $HOME/3proxy_restart.sh" >> /etc/crontab

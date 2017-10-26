34e2ee55459df8c5844951ef1c8a05d1cbf230a5
fakegit <fakegit@outlook.com>
2017-10-26 17:38:11 +0800

    Update restart_ff.bat

--- firefox/restart_ff.bat
+++ firefox/restart_ff.bat
@@ -1,11 +1,11 @@
 @echo off
 :loop
-start "" "C:\Program Files (x86)\Mozilla Firefox\firefox.exe -no-remote"
-timeout 3600 /NOBREAK
 cls
 taskkill /F /IM Firefox.exe
 cls
 taskkill /F /IM crashreporter.exe
 cls
 taskkill /F /IM plugin-container.exe
+start "" "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" -no-remote
+timeout 3600 /NOBREAK
 goto loop

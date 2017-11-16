d63bf6d1c2540b5d39a502bbf105c0f177821656
fakegit <fakegit@outlook.com>
2017-11-16 23:18:00 +0800

    Create ubuntu16.sh

---
+++ vnc/ubuntu16.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-vnc-on-ubuntu-16-04
+
+sudo apt install xfce4 xfce4-goodies tightvncserver
+vncserver
+vncserver -kill :1
+mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
+
+cat > ~/.vnc/xstartup << EOF #!/bin/bash
+xrdb $HOME/.Xresources
+startxfce4 &
+EOF
+
+sudo chmod +x ~/.vnc/xstartup
+
+cat > /etc/systemd/system/vncserver@.service << EOF [Unit]
+Description=Start TightVNC server at startup
+After=syslog.target network.target
+
+[Service]
+Type=forking
+User=$USER
+PAMName=login
+PIDFile=/${USER}/.vnc/%H:%i.pid
+ExecStartPre=-/usr/bin/vncserver -kill :%i > /dev/null 2>&1
+ExecStart=/usr/bin/vncserver -depth 24 -geometry 1280x800 :%i
+ExecStop=/usr/bin/vncserver -kill :%i
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+
+sudo systemctl enable vncserver@18.service
+sudo systemctl start vncserver@18
+sudo systemctl status vncserver@18

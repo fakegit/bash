f6b97124d250c7ba884558768e5b785646bcb736
fakegit <fakegit@outlook.com>
2018-06-16 02:26:52 +0800

    Create 3proxy_on.sh

---
+++ misc/3proxy_on.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+while [ 1 ]
+do
+  ps -ef|grep 3proxy|grep -v grep
+  if [ $? -ne 0 ]
+  then
+  echo "start 3proxy ..."
+  bash 3proxy_restart.sh
+  else
+  echo "3proxy is running."
+  fi
+  sleep 600
+done
